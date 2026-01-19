8cb59fdfd663eb7533280a01dadbe494548a447a
Paul <paul.sohler@outlook.com>
2026-01-19 13:37:55 +0100

    add fact checking table

--- backend/src/app/service/trust/author.py
+++ backend/src/app/service/trust/author.py
@@ -1,9 +1,18 @@
-from src.modules.author_expertise.author_expertise_classifier import assess_author_expertise
+from src.modules.author_expertise.author_expertise_classifier import assess_author_expertise, AuthorExpertiseResult
 
 
 def analyze_author(article):
-    return assess_author_expertise(
-        article.content_html,
-        article.author,
-        str(article.url),
+    #return assess_author_expertise( TODO
+    #    article.content_html,
+    #    article.author,
+    #    str(article.url),
+    #)
+    return AuthorExpertiseResult(
+        author="Test Author",
+        article_url="http",
+        publisher_domain="spiegel.de",
+        field="Geopolitics",
+        label="field_expert",
+        confidence=0.9,
+        explanation="Test explanation"
     )
